3e374a9ad54187075d49ddd96a03dee4130abd91
Dawin Kweon <dawin.kweon@NZMB2200285.local>
2023-04-30 00:03:32 +1200

    improvement: add timer metrics for invoice creation

--- src/services/invoice/InvoiceDocumentGenerator.ts
+++ src/services/invoice/InvoiceDocumentGenerator.ts
@@ -1,4 +1,5 @@
 import { Invoice, Type } from "@/models/Invoice";
+import { timed } from "../performanceUtils";
 
 const fs = require("fs");
 const pdf = require("pdfjs");
@@ -14,65 +15,68 @@ const getBottomPadding = (units: number = 1): object => {
 
 class InvoiceDocumentGenerator {
   public async generateInvoice(invoice: Invoice): Promise<string> {
-    const doc = new pdf.Document({
-      font: require("pdfjs/font/Times-Roman"),
-      fontSize: 14,
-      padding: 60,
-      lineHeight: 2,
-    });
+    const fn = async () => {
+      const doc = new pdf.Document({
+        font: require("pdfjs/font/Times-Roman"),
+        fontSize: 14,
+        padding: 60,
+        lineHeight: 2,
+      });
 
-    const fileName = InvoiceDocumentGenerator.generateFileName(invoice.type);
-    const filePath = `/tmp/${fileName}.pdf`;
-    doc.pipe(fs.createWriteStream(filePath));
+      const fileName = InvoiceDocumentGenerator.generateFileName(invoice.type);
+      const filePath = `/tmp/${fileName}.pdf`;
+      doc.pipe(fs.createWriteStream(filePath));
 
-    doc.cell("2/10 Mannering Place, Hillcrest, Auckland", centerTextAlign);
-    doc.cell("Mobile: 027 443 4087", centerTextAlign);
-    doc.cell(`Date: ${invoice.date}`, {
-      ...centerTextAlign,
-      ...getBottomPadding(),
-    });
-    doc.cell("", centerTextAlign);
+      doc.cell("2/10 Mannering Place, Hillcrest, Auckland", centerTextAlign);
+      doc.cell("Mobile: 027 443 4087", centerTextAlign);
+      doc.cell(`Date: ${invoice.date}`, {
+        ...centerTextAlign,
+        ...getBottomPadding(),
+      });
+      doc.cell("", centerTextAlign);
 
-    if (invoice.type === Type.INVOICE) {
-      doc.cell("TAX INVOICE", centerTextAlign);
-      doc.cell("GST No: 81-142-408", centerTextAlign);
-    } else {
-      doc.cell((invoice.type as string).toUpperCase(), centerTextAlign);
-    }
-    doc.cell("", getBottomPadding());
+      if (invoice.type === Type.INVOICE) {
+        doc.cell("TAX INVOICE", centerTextAlign);
+        doc.cell("GST No: 81-142-408", centerTextAlign);
+      } else {
+        doc.cell((invoice.type as string).toUpperCase(), centerTextAlign);
+      }
+      doc.cell("", getBottomPadding());
 
-    doc.cell("Services provided to:", centerTextAlign);
-    if (invoice.clientName != null) {
-      doc.cell(invoice.clientName, centerTextAlign);
-    }
-    doc.cell(invoice.address, {
-      ...centerTextAlign,
-      ...getBottomPadding(),
-    });
+      doc.cell("Services provided to:", centerTextAlign);
+      if (invoice.clientName != null) {
+        doc.cell(invoice.clientName, centerTextAlign);
+      }
+      doc.cell(invoice.address, {
+        ...centerTextAlign,
+        ...getBottomPadding(),
+      });
 
-    doc.cell(`Our service: ${invoice.serviceDesc}`, {
-      ...centerTextAlign,
-      ...getBottomPadding(),
-    });
+      doc.cell(`Our service: ${invoice.serviceDesc}`, {
+        ...centerTextAlign,
+        ...getBottomPadding(),
+      });
 
-    if (invoice.isIncludeGst) {
-      doc.cell(
-        `Total cost: $${invoice.cost.toFixed(2)} (incl. GST)`,
-        centerTextAlign
-      );
-    } else {
-      doc.cell(`Our Cost: $${invoice.cost.toFixed(2)}`, centerTextAlign);
-      doc.cell(`GST: $${invoice.gstAmount.toFixed(2)}`, centerTextAlign);
-      doc.cell(`Total: $${invoice.totalCost.toFixed(2)}`, centerTextAlign);
-    }
+      if (invoice.isIncludeGst) {
+        doc.cell(
+          `Total cost: $${invoice.cost.toFixed(2)} (incl. GST)`,
+          centerTextAlign
+        );
+      } else {
+        doc.cell(`Our Cost: $${invoice.cost.toFixed(2)}`, centerTextAlign);
+        doc.cell(`GST: $${invoice.gstAmount.toFixed(2)}`, centerTextAlign);
+        doc.cell(`Total: $${invoice.totalCost.toFixed(2)}`, centerTextAlign);
+      }
 
-    doc.cell("", getBottomPadding(2));
+      doc.cell("", getBottomPadding(2));
 
-    doc.cell("Young's Garden Services Ltd", rightTextAlign);
-    doc.cell("ASB: 12-3053-0466668-00", rightTextAlign);
+      doc.cell("Young's Garden Services Ltd", rightTextAlign);
+      doc.cell("ASB: 12-3053-0466668-00", rightTextAlign);
 
-    await doc.end();
-    return filePath;
+      await doc.end();
+      return filePath;
+    };
+    return timed(fn, "generateInvoice");
   }
 
   private static generateFileName(type: Type): string {
--- src/services/mail/EmailService.ts
+++ src/services/mail/EmailService.ts
@@ -1,4 +1,5 @@
 import { Invoice } from "@/models/Invoice";
+import { timed } from "../performanceUtils";
 const nodeMailer = require("nodemailer");
 
 const mailAddress = process.env.MAIL_ADDRESS;
@@ -6,38 +7,41 @@ const mailPassword = process.env.MAIL_PASSWORD;
 
 class EmailService {
   public async sendMail(filePath: string, invoice: Invoice): Promise<any> {
-    const mail = nodeMailer.createTransport({
-      host: "smtp-mail.outlook.com",
-      port: 25,
-      secure: false, // upgrade later with STARTTLS
-      auth: {
-        user: mailAddress,
-        pass: mailPassword,
-      },
-    });
-
-    const { email, emailSubject, emailBody } = invoice;
-
-    const message = {
-      from: mailAddress,
-      to: email,
-      subject: emailSubject,
-      text: emailBody,
-      html: "",
-      attachments: [
-        {
-          filename: `${
-            invoice.type.charAt(0).toUpperCase() +
-            invoice.type.slice(1).toLocaleLowerCase()
-          } - Young's Garden Services.pdf`,
-          path: filePath,
+    const fn = async () => {
+      const mail = nodeMailer.createTransport({
+        host: "smtp-mail.outlook.com",
+        port: 25,
+        secure: false, // upgrade later with STARTTLS
+        auth: {
+          user: mailAddress,
+          pass: mailPassword,
         },
-      ],
+      });
+
+      const { email, emailSubject, emailBody } = invoice;
+
+      const message = {
+        from: mailAddress,
+        to: email,
+        subject: emailSubject,
+        text: emailBody,
+        html: "",
+        attachments: [
+          {
+            filename: `${
+              invoice.type.charAt(0).toUpperCase() +
+              invoice.type.slice(1).toLocaleLowerCase()
+            } - Young's Garden Services.pdf`,
+            path: filePath,
+          },
+        ],
+      };
+
+      console.log(`Sending mail with message: ${JSON.stringify(message)}`);
+
+      return mail.sendMail(message);
     };
-
-    console.log(`Sending mail with message: ${JSON.stringify(message)}`);
-
-    return mail.sendMail(message);
+    return timed(fn, "sendEmail");
   }
 }
 
